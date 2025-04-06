ade286b70e946080e803c37a1588c6681945ab48
Ornitorrinco115 <92495858+Ornitorrinco115@users.noreply.github.com>
2025-04-06 15:13:21 -0300

    Add files via upload

---
+++ biblia/script.js
@@ -0,0 +1,26 @@
+// Função para consumir o JSON e exibir o versículo do dia
+function displayVerse() {
+    const date = new Date();
+    const day = date.getDate(); // Obter o dia atual
+
+    // Fazer o fetch para pegar os dados do arquivo JSON
+    fetch('versiculos.json')
+        .then(response => response.json())
+        .then(data => {
+            // Acessar os versículos do JSON
+            const verses = data.versiculos;
+
+            // Encontrar o versículo do dia
+            const verseOfTheDay = verses[day - 1].texto; // day - 1 porque o array começa em 0
+
+            // Exibir o versículo do dia na página
+            document.getElementById("verse").textContent = verseOfTheDay;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo JSON:', error);
+            document.getElementById("verse").textContent = 'Erro ao carregar versículo do dia.';
+        });
+}
+
+// Chamar a função quando a página carregar
+window.onload = displayVerse;
